8ec151276e20317a822cc8265e9474ab6fda29ff
Jader Raniere Albrez <jader.ralbrez@gmail.com>
2022-05-21 10:56:43 -0400

    Commit final
    Organizando as divs para limpar ao clicar em botão

--- script.js
+++ script.js
@@ -1,18 +1,53 @@
-console.log("Javascript Carregado");
-
 function validaCPF(cpf){
 
+    if(cpf.length != 11){
+        return false;
+    }else{
+        var numeros = cpf.substring(0, 9);
+        var digitos = cpf.substring(9);
+
+        var soma = 0;
+        for (var i = 10; i > 1; i--){
+            soma += numeros.charAt(10-i)*i;
+        }
+
+        var resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+
+        //Validação do primeiro dígito
+        if(resultado != digitos.charAt(0)){
+            return false;
+        }
+
+        soma = 0;
+        numeros=cpf.substring(0, 10);
+
+        for(var k = 11; k > 1; k--){
+            soma += numeros.charAt(11 - k) *k;
+        }
+
+        resultado = soma % 11 > 2 ? 0 : 11 - (soma % 11);
+
+        //Validação do segundo dígito
+        if(resultado != digitos.charAt(1)){
+            return false;
+        }
+
+        return true;
+        
+    }
 }
 
 function validacao(){
     console.log("Iniciando validação de cpf");
     var cpf = document.getElementById('cpf_digitado').value;
-    console.log(cpf);
+    document.getElementById('success').style.display = 'none';
+    document.getElementById('error').style.display = 'none';
+  
 
     var resultadoValidacao = validaCPF(cpf);
     if(resultadoValidacao == true){
-        document.getElementById('sucess').style.display = block;
+        document.getElementById('success').style.display = 'block';
     }else{
-        document.getElementById('error').style.display = block;
+        document.getElementById('error').style.display = 'block';
     }
 }
